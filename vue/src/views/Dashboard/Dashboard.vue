906e0a991ec74832ba25f83edb92a1822b9274f9
Daesub Kim <rano@bluehack.net>
2017-08-01 00:06:06 +0900

    Resolved [WHATSIT-26] : Implemented loading nextImage feature / Test images added

--- vue/src/views/Dashboard/Dashboard.vue
+++ vue/src/views/Dashboard/Dashboard.vue
@@ -6,12 +6,16 @@
         ref="cropper"
         :guides="true"
         :autoCrop="false"
+        :background="true"
+        :minContainerWidth="500"
+        :minContainerHeight="500"
+        :minCanvasWidth="500"
+        :minCanvasHeight="500"
         :view-mode="2"
         :drag-mode="crop"
-        :background="true"
         :src="imgSrc"
         :cropmove="cropImage">
-        alt="Source Image">
+        alt="이미지 불러오는중...">
       </VueCropper>
     </div>
 
@@ -20,7 +24,7 @@
       :src="cropImg"
       style="object-fit:contain; width: 500px; height: 300px; border: 1px solid gray;"
       alt="Please crop the above image."
-    />
+    />  <button @click="nextImage()">다음 사진 >></button>
     <p> X : {{ cropImgX }} / Y : {{ cropImgY }} / Width : {{ cropImgWidth }} / Height : {{ cropImgHeight }} </p>
   </div>
 </template>
@@ -29,18 +33,29 @@
 import VueCropper from 'vue-cropperjs';
 import {Dashboard} from './mixins/Dashboard'
 
+var imgList = [
+  '/static/img/bg1.jpg',
+  '/static/img/logo-w.png',
+  '/static/img/bg2.jpg',
+  '/static/img/bg3.jpeg',
+]
+var imgIndex=0
+
 export default {
   name: 'dashboard',
   mixins: [Dashboard],
+
+  beforeCreate: function () {
+
+  },
 
   components: {
     VueCropper
   },
 
-
   data: function () {
     return {
-      imgSrc: '/static/img/bg3.jpeg',
+      imgSrc: '/static/img/bg1.jpg',
       cropImg: '',
       cropImgX: '0',
       cropImgY: '0',
@@ -54,37 +69,45 @@ export default {
       const file = e.target.files[0];
 
       if (!file.type.includes('image/')) {
-        alert('Please select an image file');
+        alert('Please select an image file')
         return;
       }
 
       if (typeof FileReader === 'function') {
-        const reader = new FileReader();
+        const reader = new FileReader()
 
         reader.onload = (event) => {
-          this.imgSrc = event.target.result;
+          this.imgSrc = event.target.result
           // rebuild cropperjs with the updated source
-          this.$refs.cropper.replace(event.target.result);
-        };
+          this.$refs.cropper.replace(event.target.result)
+          console.log('Target Image...')
+          console.log(event.target.result)
+        }
 
-        reader.readAsDataURL(file);
+        reader.readAsDataURL(file)
       } else {
         alert('Sorry, FileReader API not supported');
       }
     },
+
     cropImage () {
       // get image data for post processing, e.g. upload or setting image src
-      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
-//      console.log('cropImage...')
+      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL()
       console.log(this.$refs.cropper.getData())
       this.cropImgX = Math.round(this.$refs.cropper.getData().x)
       this.cropImgY = Math.round(this.$refs.cropper.getData().y)
       this.cropImgWidth = Math.round(this.$refs.cropper.getData().width)
       this.cropImgHeight = Math.round(this.$refs.cropper.getData().height)
     },
-    rotate () {
-      // guess what this does :)
-      this.$refs.cropper.rotate(90);
+
+    nextImage () {
+      ++imgIndex
+      if (imgIndex == imgList.length) {
+        imgIndex = 0
+      }
+      this.cropImg = ''
+      this.imgSrc = imgList[imgIndex]
+      this.$refs.cropper.replace(this.imgSrc)
     }
   }
 }
